a9888c373404abfd763845b8ff65700f24069c9d
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-15 15:00:54 +0900

    Refact : Lambda 리팩토링

---
+++ src/main/java/nextstep/fp/Condition.java
@@ -0,0 +1,5 @@
+package nextstep.fp;
+
+public interface Condition {
+    boolean condition(Integer number);
+}
--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -4,53 +4,51 @@ import java.util.List;
 
 public class Lambda {
     public static void printAllOld(List<Integer> numbers) {
-        System.out.println("printAllOld");
-
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        print("printAllOld");
+        numbers.forEach(System.out::println);
     }
 
     public static void printAllLambda(List<Integer> numbers) {
-        System.out.println("printAllLambda");
-
+        print("printAllLambda");
         numbers.forEach(System.out::println);
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+        new Thread(() -> print("Hello from thread")).start();
     }
 
+
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, Lambda::getAll);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, Lambda::getEven);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, Lambda::getOverThree);
+    }
+
+    private static int sumAll(List<Integer> numbers, Condition condition) {
+        return numbers.stream()
+            .filter(condition::condition)
+            .reduce(Integer::sum).orElse(0);
+    }
+
+    private static void print(String Hello_from_thread) {
+        System.out.println(Hello_from_thread);
+    }
+
+    private static boolean getAll(Integer integer) {
+        return true;
+    }
+
+    private static boolean getEven(Integer integer) {
+        return integer % 2 == 0;
+    }
+
+    private static boolean getOverThree(Integer integer) {
+        return integer > 3;
     }
 }
