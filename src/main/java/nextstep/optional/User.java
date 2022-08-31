486d5742798f6aa2dc93ed0710108be6102fb5e4
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-31 12:40:48 +0900

    Refact : User 리팩토링

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,7 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return user != null && user.getAge() != null && (user.getAge() >= 30 && user.getAge() <= 45);
+        return Optional.ofNullable(user).map(User::getAge).filter(x -> x >= 30 && x <= 45).isPresent();
     }
 
     @Override
