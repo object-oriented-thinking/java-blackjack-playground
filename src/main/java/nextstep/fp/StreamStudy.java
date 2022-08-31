083322bc563f36a6b403b832b960b7ba7830a50b
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-31 12:04:21 +0900

    Refact : StreamStudy.java 코드 리팩토링

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -4,21 +4,18 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamStudy {
+    private static final String REGEX = "[\\P{L}]+";
 
     public static long countWords() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
             .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+        String[] words = contents.split(REGEX);
 
         long count = 0;
         for (String w : words) {
@@ -30,28 +27,12 @@ public class StreamStudy {
     public static void printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
             .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
-        // TODO 이 부분에 구현한다.
+        List<String> words = Arrays.asList(contents.split(REGEX));
 
-        Map<String, Integer> wordMap = new HashMap<>();
-
-        for (String word : words) {
-            if (!wordMap.containsKey(word)) {
-                wordMap.put(word, 0);
-            }
-            wordMap.put(word, wordMap.get(word) + 1);
-        }
-
-        List<String> collect = wordMap.entrySet()
-            .stream().filter(entry -> entry.getValue() > 12)
-            .sorted(Comparator.comparing(entry -> - entry.getValue()))
-            .map(entry -> entry.getKey().toLowerCase())
-            .distinct()
-            .collect(Collectors.toList());
-
-        List<String> subList = new ArrayList<>(collect.subList(0, 100));
-        System.out.println(subList);
-        System.out.println(subList.size());
+        words.stream().filter(w -> w.length() > 12)
+            .sorted(Comparator.comparing(String::length)
+                .reversed()).distinct().limit(100)
+            .map(String::toLowerCase).forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
